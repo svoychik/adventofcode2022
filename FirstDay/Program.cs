d6bf417b360ffcc548ef3281c19539b56cbbb982
Serhii Voichyk <svoychik@gmail.com>
2022-12-02 00:15:39 +0100

    Solved with Linq

--- FirstDay/Program.cs
+++ FirstDay/Program.cs
@@ -1,24 +1,13 @@
-﻿using var client = new HttpClient();
-var lines = File.ReadAllLines("input.txt");
-var caloriesForEachElf = GetListOfCaloriesForEveryElf(lines);
-var totalList = caloriesForEachElf.Select(x => x.Sum()).OrderDescending().ToList();
-Console.WriteLine(totalList[0] + totalList[1] + totalList[2]);
-List<List<long>> GetListOfCaloriesForEveryElf(string[] list)
-{
-    var caloriesForElves = new List<List<long>>();
-    var currList = new List<long>();
-    foreach (var item in list)
-    {
-        if (item == "")
-        {
-            currList = new List<long>();
-            caloriesForElves.Add(currList);
-        }
-            
-        else
-            currList.Add(long.Parse(item));
-    }
-
-    return caloriesForElves;
-}
+﻿var rawText = File.ReadAllText("input.txt");
+var caloriesOfElves = rawText
+    .Split("\n\n")
+    .Select(x => 
+        x.Split("\n")
+            .Select(int.Parse)
+            .Sum())
+    .OrderByDescending(x => x)
+    .Take(3)
+    .ToList();
+System.Console.WriteLine(caloriesOfElves.First());
+System.Console.WriteLine(caloriesOfElves.Sum());
 
