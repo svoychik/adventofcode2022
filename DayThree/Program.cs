966bee3622380fa27f38a58a223b7ba9751c9a4b
Serhii Voichyk <svoychik@gmail.com>
2022-12-03 20:24:14 +0100

    Fully solved day 2

--- DayThree/Program.cs
+++ DayThree/Program.cs
@@ -1,13 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more informati
-Console.WriteLine((int)'A' - 65 + 26 + 1);
-Console.WriteLine((int)'a' - (97) + 1);
-
-static int GetPriority(char c) => char.IsLower(c) ? c - (97) + 1 : c - 65 + 26 + 1;
+﻿static int GetPriority(char c) => char.IsLower(c) ? c - (97) + 1 : c - 65 + 27;
+var allLines = File.ReadAllLines("input.txt");
 Console.WriteLine(
-    File.ReadAllLines("input.txt").Select(str =>
+    allLines.Select(str =>
     {
-        var str1 = str.Substring(0, str.Length / 2).ToCharArray();
-        var str2 = str.Substring(str.Length / 2).ToCharArray(); return str1.Intersect(str2).Select(GetPriority).Sum();
-
+        var chars1 = str[..(str.Length / 2)].ToCharArray();
+        var chars2 = str[(str.Length / 2)..].ToCharArray(); 
+        return chars1.Intersect(chars2).Select(GetPriority).Sum();
     }).Sum()
+);
+Console.WriteLine(
+    allLines
+        .Chunk(3)
+        .Sum(grp => grp[0]
+            .Intersect(grp[1])
+            .Intersect(grp[2])
+            .Sum(GetPriority))
 );
