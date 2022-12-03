a2234a13e29522b5822408f696285055cd9567d9
Serhii Voichyk <svoychik@gmail.com>
2022-12-03 19:51:23 +0100

    Day 3, task 1

--- DayThree/Program.cs
+++ DayThree/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more informati
+Console.WriteLine((int)'A' - 65 + 26 + 1);
+Console.WriteLine((int)'a' - (97) + 1);
 
-Console.WriteLine("Hello, World!");
+static int GetPriority(char c) => char.IsLower(c) ? c - (97) + 1 : c - 65 + 26 + 1;
+Console.WriteLine(
+    File.ReadAllLines("input.txt").Select(str =>
+    {
+        var str1 = str.Substring(0, str.Length / 2).ToCharArray();
+        var str2 = str.Substring(str.Length / 2).ToCharArray(); return str1.Intersect(str2).Select(GetPriority).Sum();
+
+    }).Sum()
+);
