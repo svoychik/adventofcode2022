f9b1e5f0f943d446040c2237845a9bda1dafb35b
Serhii Voichyk <svoychik@gmail.com>
2022-12-04 14:09:16 +0100

    Refactored 4 a bit

--- DayFour/Program.cs
+++ DayFour/Program.cs
@@ -2,19 +2,13 @@
 Console.WriteLine(
     lines
         .Count(pairs =>
-        {
-            var (p1, p2) = (pairs[0], pairs[1]);
-            return p1.FullyContains(p2)
-                   || p2.FullyContains(p1);
-        })
+            pairs[0].FullyContains(pairs[1])
+            || pairs[1].FullyContains(pairs[0])
+            )
 );
 Console.WriteLine(
     lines
-        .Count(pairs =>
-        {
-            var (p1, p2) = (pairs[0], pairs[1]);
-            return p1.Overlaps(p2);
-        })
+        .Count(pairs => pairs[0].Overlaps(pairs[1]))
 );
 
 public static class Ext
@@ -38,9 +32,8 @@ public static class Ext
     //(2-6), (4-8); (4-8), (2-6)
     public static bool Overlaps(this (int start, int end) pair1, (int start, int end) pair2) =>
         pair1.start <= pair2.end && pair1.end >= pair2.start;
-        
-    //curr: 4-9
-    //pair: 5-9
+
+    //(4-9), (5,9)
     public static bool FullyContains(this (int start, int end) curr, (int start, int end) pair) =>
         curr.start <= pair.start && curr.end >= pair.end;
 }
