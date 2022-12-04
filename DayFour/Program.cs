84bc25e657b2c42d32ed812f57cec99cd4019280
Serhii Voichyk <svoychik@gmail.com>
2022-12-04 14:06:42 +0100

    Fully solved day 4

--- DayFour/Program.cs
+++ DayFour/Program.cs
@@ -1,12 +1,19 @@
-﻿var lines = File.ReadAllLines("input.txt");
+﻿var lines = File.ReadAllLines("input.txt").ParsePairs().ToList();
 Console.WriteLine(
     lines
-        .ParsePairs()
         .Count(pairs =>
         {
             var (p1, p2) = (pairs[0], pairs[1]);
             return p1.FullyContains(p2)
                    || p2.FullyContains(p1);
+        })
+);
+Console.WriteLine(
+    lines
+        .Count(pairs =>
+        {
+            var (p1, p2) = (pairs[0], pairs[1]);
+            return p1.Overlaps(p2);
         })
 );
 
@@ -28,6 +35,10 @@ public static class Ext
             });
     }
 
+    //(2-6), (4-8); (4-8), (2-6)
+    public static bool Overlaps(this (int start, int end) pair1, (int start, int end) pair2) =>
+        pair1.start <= pair2.end && pair1.end >= pair2.start;
+        
     //curr: 4-9
     //pair: 5-9
     public static bool FullyContains(this (int start, int end) curr, (int start, int end) pair) =>
