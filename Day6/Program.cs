f9699e899de4e58e216ba1eb32b81c22a15b540a
Serhii Voichyk <svoychik@gmail.com>
2022-12-08 23:34:12 +0100

    Day 6 simplified

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -1,26 +1,18 @@
 ﻿using static System.Console;
 
-WriteLine(SequenceOfNotRepeatingChars(
+WriteLine(NumberOfProcessedCharsToFindUniqueSeq(
     File.ReadAllText("input.txt"), 
     4));
-WriteLine(SequenceOfNotRepeatingChars(
+WriteLine(NumberOfProcessedCharsToFindUniqueSeq(
     File.ReadAllText("input.txt"),
     14));
 
-int SequenceOfNotRepeatingChars(string str, int nDistinctChars)
+int NumberOfProcessedCharsToFindUniqueSeq(string str, int nDistinctChars)
 {
-    for (int i = 0; i < str.Length; i++)
+    for (var i = 0; i < str.Length; i++)
     {
-        var set = new HashSet<char>(){ str[i] };
-        for (var j = i + 1; j < str.Length; j++)
-        {
-            if(set.Contains(str[j]))
-                break;
-            set.Add(str[j]);
-            if (set.Count == 4)
-                return j + 1;
-        }
+        if (str.Substring(i, nDistinctChars).ToCharArray().Distinct().Count().Equals(nDistinctChars))
+            return i + nDistinctChars;
     }
-
     return -1;
 }
