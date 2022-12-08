75dde8be4cbe7cc43962906a3280958011f57aea
Serhii Voichyk <svoychik@gmail.com>
2022-12-08 23:25:20 +0100

    Solved day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+WriteLine(SequenceOfNotRepeatingChars(
+    File.ReadAllText("input.txt"), 
+    4));
+WriteLine(SequenceOfNotRepeatingChars(
+    File.ReadAllText("input.txt"),
+    14));
+
+int SequenceOfNotRepeatingChars(string str, int nDistinctChars)
+{
+    for (int i = 0; i < str.Length; i++)
+    {
+        var set = new HashSet<char>(){ str[i] };
+        for (var j = i + 1; j < str.Length; j++)
+        {
+            if(set.Contains(str[j]))
+                break;
+            set.Add(str[j]);
+            if (set.Count == 4)
+                return j + 1;
+        }
+    }
+
+    return -1;
+}
